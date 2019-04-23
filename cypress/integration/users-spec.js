1198f96bf2ee6ef2bb8cf09eb79cb455097f95e6
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-04-23 14:26:41 -0400

    chore: upgrade to Cypress 3.2.0

--- cypress/integration/users-spec.js
+++ cypress/integration/users-spec.js
@@ -8,40 +8,45 @@ describe('users', () => {
   })
 
   it('loads list of posts', () => {
-    cy.get('li').should('have.length', 3)
+    cy.get('li', { timeout: 10000 }).should('have.length', 3)
   })
 
   it('can inspect real data in XHR', () => {
     cy.server()
     cy.route('/users?_limit=3').as('users')
-    cy.wait('@users').its('response.body').should('have.length', 3)
+    cy.wait('@users')
+      .its('response.body')
+      .should('have.length', 3)
   })
 
   it('can display mock XHR response', () => {
     cy.server()
     const users = [{ id: 1, name: 'foo' }]
     cy.route('GET', '/users?_limit=3', users).as('users')
-    cy.get('li').should('have.length', 1).first().contains('foo')
+    cy.get('li')
+      .should('have.length', 1)
+      .first()
+      .contains('foo')
   })
 
   it('can inspect mocked XHR', () => {
     cy.server()
     const users = [{ id: 1, name: 'foo' }]
     cy.route('GET', '/users?_limit=3', users).as('users')
-    cy.wait('@users').its('response.body').should('deep.equal', users)
+    cy.wait('@users')
+      .its('response.body')
+      .should('deep.equal', users)
   })
 
   it('can delay and wait on XHR', () => {
     cy.server()
     const users = [{ id: 1, name: 'foo' }]
-    cy
-      .route({
-        method: 'GET',
-        url: '/users?_limit=3',
-        response: users,
-        delay: 1000
-      })
-      .as('users')
+    cy.route({
+      method: 'GET',
+      url: '/users?_limit=3',
+      response: users,
+      delay: 1000
+    }).as('users')
     cy.get('li').should('have.length', 0)
     cy.wait('@users')
     cy.get('li').should('have.length', 1)
