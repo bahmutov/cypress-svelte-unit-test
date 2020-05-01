8342bf943c18baacd1e04e056e68ed67e7207c85
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-04-30 23:08:16 -0400

    add global registration test

---
+++ cypress/components/global-handlers/GlobalEventHandlers.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let docClicks = 0;
+  export let winClicks = 0;
+  document.addEventListener("click", e => docClicks++);
+  window.addEventListener("click", e => winClicks++);
+</script>
+
+<div>Doc click: {docClicks}</div>
+<div>Win click: {winClicks}</div>
