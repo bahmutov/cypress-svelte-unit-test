e9a45206331815f3a89bead73213d0b51557a46e
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-05-07 09:51:22 -0400

    move webpack config out

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,11 +1,4 @@
-// const webpackPreprocessor = require('@cypress/webpack-preprocessor')
-// const options = {
-//   // send in the options from your webpack.config.js, so it works the same
-//   // as your app's code
-//   webpackOptions: require('../webpack.config.js'),
-// }
-
 module.exports = (on) => {
-  // on('file:preprocessor', webpackPreprocessor(options))
+  // https://github.com/bahmutov/cy-rollup
   on('file:preprocessor', require('@bahmutov/cy-rollup'))
 }
