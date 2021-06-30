2ca28d6255c13671ad2b9a41f02d08f48ef621da
John Schottler <jcschottler1991@gmail.com>
2021-06-30 10:17:57 -0600

    add test that asserts my change works

--- cypress/components/styles/styles-spec.js
+++ cypress/components/styles/styles-spec.js
@@ -28,4 +28,19 @@ describe('Styles', () => {
     })
     cy.get('body').should('have.css', 'background-color', 'rgb(0, 255, 255)')
   })
+  
+  it('adds CSS file and resolves a svelte component', () => {
+    // Regression test for https://github.com/bahmutov/cypress-svelte-unit-test/issues/250.
+    // Basically, `mount` was not returning the promise that returns the svelte component instance if using `cssFile` option.
+    // So the test couldn't access the instance to call svelte's `$set` method, for instance.
+    mount(Component, null, {
+      cssFile: 'cypress/components/styles/app.css',
+    }).then((componentInstance) => {
+      expect(componentInstance.$set != null).to.eq(
+        true,
+        'The resolved object should be a svelte component instance.',
+      )
+    })
+    cy.get('body').should('have.css', 'background-color', 'rgb(0, 255, 255)')
+  })
 })
